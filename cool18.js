28a4fb9d48c02484c3a8e9c1eed8288dbfa86248
Ken Leung <github@iken.be>
2015-07-03 17:43:52 +0800

    cool18 => module

--- cool18.js
+++ cool18.js
@@ -1,17 +1,14 @@
-//module
-
 function cool18(){
+    document.title = "Text Converter";
     var container = document.getElementById("container");
     container.innerHTML = "<textarea rows=\"30\" cols=\"80\" id=\"textA\">Paste Your Text Here.</textarea><textarea rows=\"30\" cols=\"80\" id=\"textB\"></textarea><input type=\"button\" value=\"Convert\" id=\"convert\">"
-
     var textATemp = document.getElementById("textA"),
         textBTemp = document.getElementById("textB");
-    //if(textBTemp.readOnly) textBTemp.readOnly = true;
     function process(){
         textBTemp.value = textATemp.value.replace(/\n\n/g,"==i==");
         textBTemp.value = textBTemp.value.replace(/\n/g,"");
         textBTemp.value = textBTemp.value.replace(/==i==/g,"\n");
     };
     document.getElementById('convert').onclick = function(){process()};
-}
+};
 cool18();
